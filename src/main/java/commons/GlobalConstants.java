77ab8fa1e1bc806f081f9f230dcf031e618b0836
hoanglh <le.h.hoang11@gmail.com>
2024-10-14 23:39:11 +0700

    update xml file name

--- src/main/java/commons/GlobalConstants.java
+++ src/main/java/commons/GlobalConstants.java
@@ -10,8 +10,4 @@ public class GlobalConstants {
     public static final int THREE_SECONDS = 3;
     public static final long FIVE_SECONDS = 5;
     public static final long LONG_TIMEOUT = 30;
-    public static final String ECICS_DEV_ENV = "https://ecics-dev.tdt.asia/";
-    public static final String ECICS_UAT_ENV = "https://www.triceratopdev.com/";
-    public static final String ECICS_PROD_ENV = "https://www.ecics.com/";
-    public static final String GMAIL = "https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ifkv=ARZ0qKKtcDC-Q9r_cx3i4fVbOqEoTdrtyxThf5rbXbNMHbHUb5hu91lZ7vDkNWHQKN3iiEffx0KXKg&rip=1&sacu=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-217130738%3A1711339550357773&theme=mn&ddm=0";
 }
